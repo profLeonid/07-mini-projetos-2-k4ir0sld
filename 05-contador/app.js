484832cfe0e1ebbf4c11fbd322b69a58f2136f83
lucasaleduarte <lucasaleduarte0@gmail.com>
2026-03-24 11:28:00 -0300

    Atividade do Contador

---
+++ 05-contador/app.js
@@ -0,0 +1,99 @@
+'use strict'
+
+//Função para listar numeros
+const criarListaNumeros = function(quantidade){
+    let listaNumero = []
+
+    for(let i = 1; i <= quantidade; i++){
+        listaNumero.push(i)
+    }
+    return listaNumero
+}
+
+//Função para listar numeros pares
+const criarListaPares = function(numero){
+    let listaPar = []
+    let valor = 0 
+
+    for(let i = 2; i <= numero; i++){
+        valor = valor + 2   
+        listaPar.push(valor)
+    }
+    return listaPar
+
+}
+
+//Função para listar numeros impares
+const criarListaImpares = function(numero){
+    let listaImpar = []
+
+    for(let i = 1; i <= numero; i+=2){   
+        listaImpar.push(i)
+    }
+    return listaImpar
+
+}
+
+//Função para listar a quantidade de numeros multiplicados por 5 (Tipo a tabuada do 5)
+const criarListaMulti5 = function(numero){
+    let listaMulti = []
+    let valor = 0
+
+    for(let i = 1; i <= numero; i++){
+        valor = valor + 5
+        listaMulti.push(valor)
+    }
+    return listaMulti
+}
+
+//Função para listar numeros potenciados a 2 pela quantidade de numeros
+const criarListaPotencia2 = function(numero){
+    let listaPot = []
+    let valor = 2
+    let resultado
+    for(let i = 0; i <= numero; i++){
+        resultado = (valor**i)
+        listaPot.push(resultado)
+    }
+    return listaPot
+}
+
+const criarLinha = function(numero, par, impar, multi5, pot2){
+    const tabela = document.getElementById("tabela")
+    const tr = document.createElement("tr")
+
+    const tdNum = document.createElement("td")
+    tdNum.textContent = numero
+
+    const tdPar = document.createElement("td")
+    tdPar.textContent = par
+
+    const tdImpar = document.createElement("td")
+    tdImpar.textContent = impar
+
+    const tdMulti5 = document.createElement("td")
+    tdMulti5.textContent = multi5
+
+    const tdPot2 = document.createElement("td")
+    tdPot2.textContent = pot2
+
+    tr.replaceChildren(tdNum, tdPar, tdImpar, tdMulti5, tdPot2)
+    tabela.appendChild(tr)
+
+}
+
+const handleClick = function(){
+    const quantidade = Number(document.getElementById("quantidade").value)
+
+    const listaNumeros = criarListaNumeros(quantidade)
+    const listaPar = criarListaPares(quantidade)
+    const listaImpar = criarListaImpares(quantidade)
+    const listaMulti5 = criarListaMulti5(quantidade)
+    const listaPot2 = criarListaPotencia2(quantidade)
+
+    for(let i = 0; i < quantidade; i++){
+        criarLinha(listaNumeros[i], listaPar[i], listaImpar[i], listaMulti5[i], listaPot2[i])
+    }
+
+
+}
